edfe53aebaf3297f188adf44525ee5101c2672ca
dongdong.dou <dongdong.dou@ihandysoft.com>
2017-10-25 17:51:39 +0800

    String of link

--- app/src/main/java/com/winiter/everyutils/MainActivity.java
+++ app/src/main/java/com/winiter/everyutils/MainActivity.java
@@ -1,7 +1,10 @@
 package com.winiter.everyutils;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvLink = (TextView) findViewById(R.id.tv_link);
+        String html = getString(R.string.string_of_link);
+        CharSequence charSequence = Html.fromHtml(html);
+        tvLink.setMovementMethod(LinkMovementMethod.getInstance());
+        tvLink.setText(charSequence);
     }
 }
